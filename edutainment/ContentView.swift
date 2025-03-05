4f83e86f48720f9c6fae538299889475262a9a04
Marko Živanović <zivanovic.marko@icloud.com>
2025-03-05 20:57:58 +0100

    Educational App

---
+++ edutainment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  edutainment
+//
+//  Created by Marko Zivanovic on 5.3.25..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Marko Zivanovic on 5.3.25..
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
