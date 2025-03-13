4ba22daddd51363da99a880e22381664b2fc56fa
Marko Živanović <zivanovic.marko@icloud.com>
2025-03-13 14:24:53 +0100

    Number Of A Question

--- edutainment/ContentView.swift
+++ edutainment/ContentView.swift
@@ -10,30 +10,44 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var firstFactor = 1
+    @State private var questionNumber = 1
     
     let firstFactors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     
     var body: some View {
-        NavigationStack {
-                Form {
-                    Section("Select first factor.") {
-                        Picker("First factor", selection: $firstFactor) {
-                            ForEach(firstFactors, id: \.self) { factor in
-                                Text("\(factor)")
+        ZStack {
+            VStack {
+                NavigationStack {
+                        Form {
+                            Section("Select first factor.") {
+                                Picker("First factor", selection: $firstFactor) {
+                                    ForEach(firstFactors, id: \.self) { factor in
+                                        Text("\(factor)")
+                                    }
+                                }
+                            }
+                            
+                            Section("How many questions do you choose?") {
+                                Picker("Choose number of a question", selection: $questionNumber) {
+                                    ForEach(1..<11) {
+                                        Text("\($0)")
+                                    }
+                                }
+                                .pickerStyle(.navigationLink)
+                            }
+                            
+                            Section("Section 3.") {
+                                
                             }
                         }
+                        .navigationTitle("Edutainment")
                     }
-                    
-                    Section("Section 2.") {
-                        
-                    }
-                    
-                    Section("Section 3.") {
-                        
-                    }
-                }
-                .navigationTitle("Game")
             }
+            HStack {
+                
+            }
+        }
+
 
         }
 
